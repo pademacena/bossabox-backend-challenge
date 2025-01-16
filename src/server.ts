ee2b1d6d5f682d2157a544ee888ba5567f6037b0
Paulo Augusto de Macena Pereira <pademacena@gmail.com>
2025-01-15 23:35:58 -0300

    fix: port adjust

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-app.listen(process.env.PORT || 3333, function (this: any) {
+app.listen(process.env.PORT || 3000, function (this: any) {
   console.log(
     "Server listening on port %d in %s mode",
     this.address().port,
